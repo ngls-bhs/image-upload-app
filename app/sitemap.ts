9255a4e347e269b4ff1e597b104d63f1f0cd6cd7
ngls-bhs <general.bhs@outlook.com>
2026-01-25 16:07:01 +0800

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,26 +5,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://image-upload.app',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
     },
     {
       url: 'https://image-upload.app/privacy',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.5,
     },
     {
       url: 'https://image-upload.app/terms',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.5,
     },
     {
       url: 'https://image-upload.app/contact',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.5,
     },
   ]
 }
